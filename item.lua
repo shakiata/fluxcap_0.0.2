1452e2df71d7ec04b1e9f574754b57f782bcdb3b
shakiata <80846748+shakiata@users.noreply.github.com>
2021-08-23 10:05:13 -0400

    working?

--- item.lua
+++ item.lua
@@ -56,3 +56,74 @@ plutoniumfuel.fuel_acceleration_multiplier = 15
 
 
 data:extend{plutonium,plutoniumfuel,recipe,plutoniumprocessing}
+
+
+data:extend(
+{
+    {
+        type = "item",
+        name = "fluxcap",
+        icon = "__fluxcap__/graphics/fluxcap.png",
+        icon_size = 64, icon_mipmaps = 4,
+        flags = {"hidden"},
+        subgroup = "other",
+        order = "t[item]-o[fluxcap]",
+        stack_size = 10,
+        place_result = "fluxcap"
+      },
+    {
+      name = "fluxcap",
+      type = "burner-generator",
+      icon = "__fluxcap__/graphics/fluxcap.png",
+      icon_size = 64, icon_mipmaps = 4,
+      flags = {"placeable-neutral","player-creation"},
+      max_health = 400,
+      dying_explosion = "medium-explosion",
+      corpse = "steam-engine-remnants",
+      collision_box = {{-1.35, -2.35}, {1.35, 2.35}},
+      selection_box = {{-1.5, -2.5}, {1.5, 2.5}},
+      max_power_output = "1.21GW",
+      minable = {mining_time = 1, result = "fluxcap"},
+      animation =
+      {
+        north = util.table.deepcopy(data.raw.generator["steam-engine"].vertical_animation),
+        east = util.table.deepcopy(data.raw.generator["steam-engine"].horizontal_animation),
+        south = util.table.deepcopy(data.raw.generator["steam-engine"].vertical_animation),
+        west = util.table.deepcopy(data.raw.generator["steam-engine"].horizontal_animation)
+      },
+      -- idle_animation can also be specified
+      burner =
+      {
+        fuel_category = "chemical",
+        effectivity = 0.5,
+        fuel_inventory_size = 1,
+        emissions_per_minute = 10,
+        smoke =
+        {
+          {
+            name = "smoke",
+            north_position = {0.9, 0.0},
+            east_position = {-2.0, -2.0},
+            deviation = {0.1, 0.1},
+            frequency = 9
+          }
+        }
+      },
+      energy_source =
+      {
+        type = "electric",
+        usage_priority = "secondary-output"
+      }
+    }})
+    
+data:extend{
+    {
+        name = "fluxcapacitorrecipe",
+        type = "recipe",
+        enabled = true,
+        ingredients = {
+            {"iron-plate", 100}
+        },
+        result = "fluxcap"
+    }
+}
